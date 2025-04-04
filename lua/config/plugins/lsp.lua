ea87969e6e80e4150f2c1f3afeb459c1a4c6bd4c
James Blash <j.w.blash@gmail.com>
2025-04-04 14:07:23 -0400

    this is broken

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,3 +1,28 @@
+local function start_godot_lsp()
+  -- Check if the LSP is already running
+
+  for _, client in pairs(vim.lsp.get_clients()) do
+    if client.name == 'Godot' then
+      return
+    end
+  end
+
+  local port = os.getenv('GDScript_Port') or '6005'
+  -- TODO(james) ncat is a dependency... check for it
+  local cmd = { 'ncat', '127.0.0.1', port }
+  local pipe = [[\\.\pipe\godot.pipe]]
+
+  vim.lsp.start({
+    name = 'Godot',
+    cmd = cmd,
+    root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+    on_attach = function(client, bufnr)
+      print("Godot LSP attached to buffer: " .. bufnr)
+      vim.api.nvim_command([[echo serverstart(']] .. pipe .. [[')]])
+    end
+  })
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -23,6 +48,7 @@ return {
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
+          print(table.concat(args, '\n'))
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           if not client then return end
 
